7f07bafedfc308d947e7de529c713355a17997f8
Prakhar Pande <pandeprakhar1801@gmail.com>
2025-05-19 23:07:22 +0530

    app data created

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import axios from "axios";
+import Loader from "./components/Loader";
+
+import Home from "./pages/Home";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  HideLoading,
+  SetPortfolioData,
+  ShowLoading,
+  ReloadData,
+} from "./redux/rootSlice";
+import Admin from "./pages/Admin";
+import Login from "./pages/Admin/Login";
 
 function App() {
+  const { loading, portfolioData, reloadData } = useSelector(
+    (state) => state.root
+  );
+  const dispatch = useDispatch();
+  const getPortfolioData = async () => {
+    try {
+      dispatch(ShowLoading());
+      const response = await axios.get("/api/portfolio/get-portfolio-data");
+      dispatch(SetPortfolioData(response.data));
+      dispatch(ReloadData(false));
+      dispatch(HideLoading());
+    } catch (error) {
+      dispatch(HideLoading());
+    }
+  };
+
+  useEffect(() => {
+    if (!portfolioData) {
+      getPortfolioData();
+    }
+  }, [portfolioData]);
+
+  useEffect(() => {
+    if (reloadData) {
+      getPortfolioData();
+    }
+  }, [reloadData]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      {loading ? <Loader /> : null}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/admin" element={<Admin />} />
+        <Route path="/admin-login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
